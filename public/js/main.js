fe42061a74cbdaa29a5477fec65007dd496e0247
lrashid2 <48542389+lrashid2@users.noreply.github.com>
2019-04-04 23:52:40 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,11 @@ var makeMove = function(algo, skill=3) {
     var move = calcBestMoveNoAB(skill, game, game.turn())[1];
   } else if (algo == 4) {
    var move = calcBestMove(skill, game, game.turn())[1];
-  } else {
-    var move = calcBestMove(skill, game, game.turn())[1];
-  } 
+  } else if (algo == 5) {
+    var move = calcBestMove_2(skill, game, game.turn())[1];
+  } else
+    var move = calcBestMove_2(skill, game, game.turn())[1];
+}
 
   // Make the calculated move
   game.move(move);
